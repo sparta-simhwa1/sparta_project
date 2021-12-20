86138fc2e2d999f5cc4560a6a4e9085a6478741d
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-12-20 20:58:39 +0900

    [Fix] 주석 처리 부분 해제 후 DB 데이터 저장 부분 추가
    주석 처리 부분 해제 후 DB 데이터 저장 부분 추가

--- flaskproject/app/mongodb.py
+++ flaskproject/app/mongodb.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from pymongo import MongoClient
 
-# data = pd.read_csv('VeganMap_csv.csv')
-# data.reset_index(inplace=True) ## index 생성
-# data_dict = data.to_dict("records") ## 데이터를 사전형으로 변환 to_dict
+data = pd.read_csv('VeganMap_csv.csv')
+data.reset_index(inplace=True) ## index 생성
+data_dict = data.to_dict("records") ## 데이터를 사전형으로 변환 to_dict
 
 client = MongoClient('localhost', 27017)
 db = client.vegan
 collection = db['vegan']
-
+collection.insert_many(data_dict)
 
 def west_north():
     users = collection.find({"$or": [{'자치구': '은평구'}, {'자치구': '서대문구'}, {'자치구': '마포구'}, {'_id': False}]})
