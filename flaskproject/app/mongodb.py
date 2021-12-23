15b9e9cb8ec8fcfe30f66fb7e388099f7d79afa6
SGC <tlsrhkdcjs12@naver.com>
2021-12-23 13:22:09 +0900

    [Fix] 주석 처리 및 정리

--- flaskproject/app/mongodb.py
+++ flaskproject/app/mongodb.py
@@ -10,31 +10,31 @@ db = client.vegan
 collection = db['vegan']
 collection.insert_many(data_dict)
 
-def west_north():
-    users = collection.find({"$or": [{'자치구': '은평구'}, {'자치구': '서대문구'}, {'자치구': '마포구'}, {'_id': False}]})
-    return users
-
-
-def east_north():
-    users = collection.find({"$or": [{'자치구': '도봉구'}, {'자치구': '노원구'}, {'자치구': '중랑구'}, {'자치구': '광진구'}, {'자치구': '동대문구'},
-                                     {'자치구': '성동구'}, {'자치구': '성북구'}, {'자치구': '강북구'}, {'_id': False}]})
-    return users
-
-
-def dosim():
-    users = collection.find({"$or": [{'자치구': '종로구'}, {'자치구': '중구'}, {'자치구': '용산구'}, {'_id': False}]})
-    return users
-
-
-def east_south():
-    users = collection.find({"$or": [{'자치구': '서초구'}, {'자치구': '강남구'}, {'자치구': '송파구'}, {'자치구': '강동구'}, {'_id': False}]})
-    return users
-
-
-def west_south():
-    users = collection.find({"$or": [{'자치구': '강서구'}, {'자치구': '양천구'}, {'자치구': '구로구'}, {'자치구': '영등포구'}, {'자치구': '동작구'},
-                                     {'자치구': '금천구'}, {'자치구': '관악구'}, {'_id': False}]})
-    return users
+# def west_north():
+#     users = collection.find({"$or": [{'자치구': '은평구'}, {'자치구': '서대문구'}, {'자치구': '마포구'}, {'_id': False}]})
+#     return users
+# 
+#
+# def east_north():
+#     users = collection.find({"$or": [{'자치구': '도봉구'}, {'자치구': '노원구'}, {'자치구': '중랑구'}, {'자치구': '광진구'}, {'자치구': '동대문구'},
+#                                      {'자치구': '성동구'}, {'자치구': '성북구'}, {'자치구': '강북구'}, {'_id': False}]})
+#     return users
+#
+#
+# def dosim():
+#     users = collection.find({"$or": [{'자치구': '종로구'}, {'자치구': '중구'}, {'자치구': '용산구'}, {'_id': False}]})
+#     return users
+#
+#
+# def east_south():
+#     users = collection.find({"$or": [{'자치구': '서초구'}, {'자치구': '강남구'}, {'자치구': '송파구'}, {'자치구': '강동구'}, {'_id': False}]})
+#     return users
+#
+#
+# def west_south():
+#     users = collection.find({"$or": [{'자치구': '강서구'}, {'자치구': '양천구'}, {'자치구': '구로구'}, {'자치구': '영등포구'}, {'자치구': '동작구'},
+#                                      {'자치구': '금천구'}, {'자치구': '관악구'}, {'_id': False}]})
+#     return users
 # users = seobuk()
 # for user in users:
 #     print(f"상호명:{user['상호명']}\n업종:{user['업종']}\n자치구:{user['자치구']}\n도로명주소:{user['도로명주소']}")
--- flaskproject/static/js/script.js
+++ flaskproject/static/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //jachigu
 const mapDosim = document.querySelector(".district#dosim");
 const mapDongnam = document.querySelector(".district#dongnam");
@@ -11,48 +10,37 @@ const detaildongnam = document.querySelector(".b");
 const detailseonam = document.querySelector(".c");
 const detailseobook = document.querySelector(".d");
 const detaildongbook = document.querySelector(".e");
-
 //function
-function toggleDosim() {
-    detaildosim.style.display = (detaildosim.style.display === "none") ? "block" : "none";
-    detaildongnam.style.display = "none";
-    detailseonam.style.display = "none";
-    detailseobook.style.display = "none";
-    detaildongbook.style.display = "none";
-}
-
+function toggleDosim(){
+  detaildosim.style.display = (detaildosim.style.display == "none") ? "block" : "none";
+  detaildongnam.style.display="none";
+  detailseonam.style.display="none";
+  detailseobook.style.display="none";
+  detaildongbook.style.display="none";}
 function toggleDongnam() {
-    detaildongnam.style.display = (detaildongnam.style.display === 'none') ? "block" : "none";
-    detaildosim.style.display = "none";
-    detailseonam.style.display = "none";
-    detailseobook.style.display = "none";
-    detaildongbook.style.display = "none";
-}
-
+  detaildongnam.style.display = (detaildongnam.style.display == 'none') ? "block" : "none";
+  detaildosim.style.display="none";
+  detailseonam.style.display="none";
+  detailseobook.style.display="none";
+  detaildongbook.style.display="none";}
 function toggleSeonam() {
-    detailseonam.style.display = (detailseonam.style.display === 'none') ? "block" : "none";
-    detaildosim.style.display = "none";
-    detaildongnam.style.display = "none";
-    detailseobook.style.display = "none";
-    detaildongbook.style.display = "none";
-}
-
+  detailseonam.style.display = (detailseonam.style.display == 'none') ? "block" : "none";
+  detaildosim.style.display="none";
+  detaildongnam.style.display="none";
+  detailseobook.style.display="none";
+  detaildongbook.style.display="none";}
 function toggleSeobook() {
-    detailseobook.style.display = (detailseobook.style.display === 'none') ? "block" : "none";
-    detaildosim.style.display = "none";
-    detaildongnam.style.display = "none";
-    detailseonam.style.display = "none";
-    detaildongbook.style.display = "none";
-}
-
+  detailseobook.style.display = (detailseobook.style.display == 'none') ? "block" : "none";
+  detaildosim.style.display="none";
+  detaildongnam.style.display="none";
+  detailseonam.style.display="none";
+  detaildongbook.style.display="none";}
 function toggleDongbook() {
-    detaildongbook.style.display = (detaildongbook.style.display === 'none') ? "block" : "none";
-    detaildosim.style.display = "none";
-    detaildongnam.style.display = "none";
-    detailseonam.style.display = "none";
-    detailseobook.style.display = "none";
-}
-
+  detaildongbook.style.display = (detaildongbook.style.display == 'none') ? "block" : "none";
+  detaildosim.style.display="none";
+  detaildongnam.style.display="none";
+  detailseonam.style.display="none";
+  detailseobook.style.display="none";}
 //click event
 
 mapDosim.addEventListener("click", toggleDosim);
@@ -60,62 +48,3 @@ mapDongnam.addEventListener("click", toggleDongnam);
 mapSeonam.addEventListener("click", toggleSeonam);
 mapSeobook.addEventListener("click", toggleSeobook);
 mapDongbook.addEventListener("click", toggleDongbook);
-=======
-//div
-const inputSearch = document.querySelector(".input-search");
-const searchButton = document.querySelector(".search-button");
-//menu button
-const menu1 = document.querySelector(".menu-1")
-const menu2 = document.querySelector(".menu-2");
-const menu3 = document.querySelector(".menu-3");
-//menu-map button 
-const dosim = document.querySelector(".dosim");
-const dongnam = document.querySelector(".dongnam")
-const seonam = document.querySelector(".seonam")
-const seobook = document.querySelector(".seobook")
-const dongbook = document.querySelector(".dongbook")
-//(menu-map button) detail
-const detaildosim = document.querySelector(".detail-dosim");
-const detaildongnam = document.querySelector(".detail-dongnam");
-const detailseonam = document.querySelector(".detail-seonam");
-const detailseobook = document.querySelector(".detail-seobook");
-const detaildongbook = document.querySelector(".detail-dongbook");
-
-
-//menumap - detail 토글
-function toggledosim() {
-    detaildosim.style.display = (detaildosim.style.display != 'none') ? "none" : "block";
-}
-
-function toggledongnam() {
-    detaildongnam.style.display = (detaildongnam.style.display != 'none') ? "none" : "block";
-}
-
-function toggleseonam() {
-    detailseonam.style.display = (detailseonam.style.display != 'none') ? "none" : "block";
-}
-
-function toggleseobook() {
-    detailseobook.style.display = (detailseobook.style.display != 'none') ? "none" : "block";
-}
-
-function toggledongbook() {
-    detaildongbook.style.display = (detaildongbook.style.display != 'none') ? "none" : "block";
-}
-
-//클릭함수
-function menu1click() {
-    console.log("hungry")
-}
-
-//이벤트추가
-dosim.addEventListener("click", toggledosim)
-dongnam.addEventListener("click", toggledongnam)
-seonam.addEventListener("click", toggleseonam)
-seobook.addEventListener("click", toggleseobook)
-dongbook.addEventListener("click", toggledongbook)
-//클릭이벤트
-menu1.addEventListener("click", menu1click)
-
-//menumap - detail 토글
->>>>>>> 5dfce128f805c86f7bf0e8070f807b27f858d5a0
