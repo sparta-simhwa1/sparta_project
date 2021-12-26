abfa418e36092386c8e68498d40bcb891d1cfc99
SGC <tlsrhkdcjs12@naver.com>
2021-12-26 21:31:28 +0900

    [Fix] 이미지 크롤링 오류 해결

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from bson.json_util import dumps
-import json
-from bson import json_util
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -37,7 +35,7 @@ def kakao_map():
 if __name__ == '__main__':
     """
     run() 함수를 사용해서 우리가 개발한 어플리케이션을 로컬서버로 실행한다. 
-     소스파일을 모듈이 아닌 python 인터프리터를 이용해서 직접 실행한다면 
+    소스파일을 모듈이 아닌 python 인터프리터를 이용해서 직접 실행한다면 
     if __name__ == '__main__': 이 문장은 우리가 실행한 서버가 현재 동작되는 유일한 서버라는 것을 보장한다.
     """
     app.run(debug=True)
